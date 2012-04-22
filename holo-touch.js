c1c4c6426c71a1d091e39c70fc61b6d686ab7556
Zachary “Gamer_Z.” Yaro <zmyaro@gmail.com>
2012-04-22 19:11:16 -0400

    Cleaned up holo-touch.js and set it to use the classList API when possible

--- holo-touch.js
+++ holo-touch.js
@@ -1,21 +1,23 @@
 function addEvents() {
 	var inputs = Array.prototype.slice.call(document.getElementsByTagName("button")).concat(Array.prototype.slice.call(document.getElementsByTagName("input")));
 	for(var i = 0; i < inputs.length; i++) {
-//		if(inputs[i].tagName.toLowerCase() == "button" || inputs[i].type.toLowerCase() == "button" || inputs[i].type.toLowerCase() == "submit" || inputs[i].type.toLowerCase() == "reset") {
+		if(["button","select"].indexOf(inputs[i].tagName.toLowerCase()) != -1 || ["button","submit","reset","radio","checkbox"].indexOf(inputs[i].type.toLowerCase()) != -1) {
 			inputs[i].addEventListener("touchstart", function(e) {
-			//inputs[i].ontouchstart = function(e) {
-				//e.srcElement.classList.add("active");
-				e.srcElement.className += " active";
+				if(!!e.srcElement.classList) { // use classList API if available
+					e.srcElement.classList.add("active");
+				} else {
+					e.srcElement.className += " active";
+				}
 			}, false);
-			//};
-			//inputs[i].addEventListener("touchend", function(e) {
-			inputs[i].ontouchend = function(e) {
-				//e.srcElement.classList.remove("active");
-				//e.srcElement.className.replace(/\s*active/g, "");
-				e.srcElement.className = "";
-			//}, false);
-			};
-//		}
+			inputs[i].addEventListener("touchend", function(e) {
+				if(!!e.srcElement.classList) { // use classList API if available
+					e.srcElement.classList.remove("active");
+				} else {
+					//e.srcElement.className.replace(/\s*active/g, "");
+					e.srcElement.className = "";
+				}
+			}, false);
+		}
 	}
 }
 
