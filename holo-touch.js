0e60486fb6808ace72c6166c281f9b623c5b3fde
Zachary Yaro <zmyaro@gmail.com>
2014-03-24 13:12:08 -0400

    Add holo-touch.js improvements and wider use of the .active class

--- holo-touch.js
+++ holo-touch.js
@@ -1,9 +1,10 @@
 window.addEventListener("load", function(e) {
 	// Do not run the script on Chrome 32 or higher.
-	if(/Chrome\/[3-9][2-9]/.test(navigator.userAgent)) {
+	/*if(/Chrome\/[3-9][2-9]/.test(navigator.userAgent)) {
 		return;
-	}
+	}*/
 	
+	// Create an array of all input elements.
 	var inputElems = Array.prototype.slice.call(document.getElementsByTagName("button")).concat(
 		Array.prototype.slice.call(document.getElementsByTagName("select"))).concat(
 		Array.prototype.slice.call(document.getElementsByTagName("input")));
@@ -13,21 +14,37 @@ window.addEventListener("load", function(e) {
 	
 	for(var i = 0; i < inputElems.length; i++) {
 		if(elemTypes.indexOf(inputElems[i].tagName.toLowerCase()) != -1 || inputTypes.indexOf(inputElems[i].type.toLowerCase()) != -1) {
-			inputElems[i].addEventListener("touchstart", function(e) {
-				if(!!e.srcElement.classList) { // use classList API if available
-					e.srcElement.classList.add("active");
-				} else {
-					e.srcElement.className += " active";
-				}
-			}, false);
-			inputElems[i].addEventListener("touchend", function(e) {
-				if(!!e.srcElement.classList) { // use classList API if available
-					e.srcElement.classList.remove("active");
-				} else {
-					e.srcElement.className = e.srcElement.className.replace(/\s*active/g, "");
-				}
-			}, false);
+			inputElems[i].addEventListener("touchstart", makeActive, false);
+			inputElems[i].addEventListener("touchend", makeInactive, false);
+			inputElems[i].addEventListener("touchcancel", makeInactive, false);
 		}
+	}
+	
+	
+	/**
+	 * Adds the “active” class to the touched element
+	 * @param {TouchEvent} e
+	 */
+	function makeActive(e) {
+		if(!!e.srcElement.classList) { // use classList API if available
+			e.srcElement.classList.add("active");
+		} else {
+			e.srcElement.className += " active";
+		}
+		
+	}
+	
+	/**
+	 * Removes the “active” class from the touched element
+	 * @param {TouchEvent} e
+	 */
+	function makeInactive(e) {
+		if(!!e.srcElement.classList) { // use classList API if available
+			e.srcElement.classList.remove("active");
+		} else {
+			e.srcElement.className = e.srcElement.className.replace(/\s*active/g, "");
+		}
+		
 	}
 }, false);
 
